a6ad2a56e0efe94af67f88f2390d692e1cd11a37
Mug9er <dhl252483@163.com>
2020-02-13 21:40:56 +0800

    updata

--- 爬取88ys的视频/Spider_video.py
+++ 爬取88ys的视频/Spider_video.py
@@ -42,16 +42,27 @@ class Spider_Video(object):
         self.name = re.findall(r'mac_name=\'(.*?)\'', response)[0]
         m3u8_unescape = re.findall(r'mac_url=unescape\(\'(.*?)\'\);', response)[0]
         m3u8_encode = unquote(m3u8_unescape, encoding='utf-8')
-        m3u8_list = re.findall(r'https://.*?playlist.m3u8', m3u8_encode)
-        m3u8_list[0] = "https%s" % m3u8_list[0].split('https')[len(m3u8_list[0].split('https'))-1]
+        m3u8_list = re.findall(r'https://.*?.m3u8', m3u8_encode)
+        header_name = m3u8_list[0].split('https')[len(m3u8_list[0].split('https'))-1].split('/')[2]
+        m3u8_list = re.findall(r'https://%s.*?.m3u8' %header_name, m3u8_encode)
         return m3u8_list
 
 
     #2 获取每个的playlist的ts
     def get_ts(self, m3u8_url):
         response = self.loop_get(m3u8_url).content.decode('utf-8')
-        ts_list = re.findall(r'out.*?.ts', response, re.S)
-        return ts_list
+        is_m3u8 = re.findall(r'.*?.m3u8', response)
+        if len(is_m3u8) != 0:
+            m3u8_url = m3u8_url.replace("index.m3u8", is_m3u8[0])
+            response = self.loop_get(m3u8_url).content.decode('utf-8')
+        ts_list = re.findall(r'.*?.ts', response)
+        new_ts_list=[]
+        for ts in ts_list:
+            new_ts_list.append(ts.split('/')[len(ts.split('/'))-1])
+        ts_list = new_ts_list
+        return ts_list,m3u8_url
+
+
     #3 多线程下载
     def download_thread(self, url, ts_list, name):
         path = "E://Video"
@@ -64,7 +75,7 @@ class Spider_Video(object):
         if not os.path.exists(path):
             os.mkdir(path)
         for ts in ts_list:
-            ts_url = url.replace("playlist.m3u8", ts)
+            ts_url = url.replace("index.m3u8", ts)
             threading.Thread(target=self.download_method, args=(ts_url, path)).start()
             time.sleep(1)
         threading.Thread(target=self.Merge_ts, args=(path, len(ts_list), name)).start()
@@ -105,7 +116,9 @@ class Spider_Video(object):
         m3u8_list = self.get_m3u8_list()
         idx = 1
         for m3u8 in m3u8_list:
-            ts_list = self.get_ts(m3u8) # 获取到第几集的ts_list
+            ts_url = self.get_ts(m3u8)
+            ts_list = ts_url[0] # 获取到第几集的ts_list
+            m3u8 = ts_url[1]
             self.download_thread(m3u8, ts_list, "第%s集" % idx)
             idx += 1
 
@@ -114,4 +127,6 @@ class Spider_Video(object):
         video_id = self.url.split('/')[len(self.url.split('/'))-1][:-6]
         url = "https://www.88ys.com/vod-play-id-%s-src-1-num-1.html" % video_id
         return url
+
+
 Spider_Video().run()
